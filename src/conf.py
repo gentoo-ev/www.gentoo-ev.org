1f2d1299c1300ac26537dbb3b12f35c11b76a21e
Ulrich Müller <ulm@gentoo.org>
2022-09-18 10:58:56 +0200

    Add favicon

--- src/conf.py
+++ src/conf.py
@@ -921,10 +921,10 @@ IMAGE_FOLDERS = {'images': 'images'}
 # FAVICONS contains (name, file, size) tuples.
 # Used to create favicon link like this:
 # <link rel="name" href="file" sizes="size"/>
-# FAVICONS = (
-#     ("icon", "/favicon.ico", "16x16"),
+FAVICONS = (
+    ("icon", "/favicon.ico", "16x16 32x32 48x48"),
 #     ("icon", "/icon_128x128.png", "128x128"),
-# )
+)
 
 # Show teasers (instead of full posts) in indexes? Defaults to False.
 # INDEX_TEASERS = False
