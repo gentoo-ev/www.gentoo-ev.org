887b68dc723a58ebfb9389ae7fe77f13acd34681
Ulrich Müller <ulm@gentoo.org>
2025-10-14 18:23:29 +0200

    New theme "gentoo-ev" identical to "bootstrap3-gradients"
    Created with:
    $ nikola theme --parent=bootstrap3-gradients -n gentoo-ev
    $ nikola theme -c base.tmpl   # after updating THEME in conf.py
    
    This will be needed for the site logo.
    
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- src/conf.py
+++ src/conf.py
@@ -161,7 +161,8 @@ NAVIGATION_ALT_LINKS = {
 
 # Name of the theme to use.
 #THEME = "bootblog4"
-THEME = "bootstrap3-gradients"
+#THEME = "bootstrap3-gradients"
+THEME = "gentoo-ev"
 
 # A theme color. In default themes, it might be displayed by some browsers as
 # the browser UI color (eg. Chrome on Android). Other themes might also use it
